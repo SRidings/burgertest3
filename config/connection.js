2d79b8a7f995a1cfc669ecf4a67c978d049820d5
Stephen Ridings <SRidings777@aol.com>
2018-09-09 02:42:29 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,20 +4,20 @@ var mysql = require ('mysql');
 
 var connection;
 
-// if (process.env.JAWSDB_URL)
-// {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }
-// else
-// {
-// 	connection = mysql.createConnection(
-// 	{
-// 		host: '127.0.0.1',
-// 		user: 'root',
-// 		password: 'root',
-// 		database: 'burgers2_db'
-// 	});
-// };
+if (process.env.JAWSDB_URL)
+{
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
+    connection = mysql.createConnection(
+{
+		host: '127.0.0.1',
+		user: 'root',
+		password: 'root',
+		database: 'burgers2_db'
+	});
+};
 
 
 connection.connect(function(err) 
